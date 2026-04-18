fd15d7a5eaeed8b6e0d137e120986da9190a3bb4
dycheng-ai <dycheng@ucsd.edu>
2026-04-17 19:25:10 -0700

    added theme and modified header spacing

--- global.js
+++ global.js
@@ -29,8 +29,44 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  // next step: create link and add it to nav
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/website/"; 
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    }
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+    a.toggleAttribute('target', a.host !== location.host);
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+let select = document.querySelector('.color-scheme select');
+
+if ('colorScheme' in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+
+select.addEventListener('change', () => {
+  document.documentElement.style.setProperty('color-scheme', select.value);
+  localStorage.colorScheme = select.value;
+});
