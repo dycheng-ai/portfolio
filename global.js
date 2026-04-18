df3f379755f69ade97c4fa4276a72ab9cd897c2f
dycheng-ai <dycheng@ucsd.edu>
2026-04-17 18:39:55 -0700

    created navigation menu on js

--- global.js
+++ global.js
@@ -3,3 +3,34 @@ console.log("IT\’S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// // returns an array of all nav links on the page
+// navLinks = $$("nav a");
+
+// // finds link to current page
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink.classList.add('current');
+// }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/dycheng-ai', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
