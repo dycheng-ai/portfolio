62efe8c1a9684ceacf896323183449593811c977
dycheng-ai <dycheng@ucsd.edu>
2026-04-17 19:32:14 -0700

    changed url base

--- global.js
+++ global.js
@@ -34,7 +34,7 @@ for (let p of pages) {
     // next step: create link and add it to nav
     const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"
-    : "/website/"; 
+    : "/portfolio/";
     if (!url.startsWith('http')) {
         url = BASE_PATH + url;
     }
